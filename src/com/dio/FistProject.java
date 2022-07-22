b624577ec6e9bff38afdcc9a4d13bdd6899bc177
nick-ju <juh_nmartins@hotmail.com>
2022-07-21 22:42:54 -0300

    first commit

---
+++ src/com/dio/FistProject.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class FistProject {
+    public static void main(String[] args) {
+        /*System.out.println("oi povo!");*/
+
+         Gato gato = new Gato();
+
+         System.out.println(gato);
+
+    }
+}
